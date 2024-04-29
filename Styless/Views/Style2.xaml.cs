f8d192d074dfce3146f8c496d2683d10de6f99f2
MirtalibEmirli <azad.sdk.85@icloud.com>
2024-04-29 12:33:57 +0400

    Add project files.

---
+++ Styless/Views/Style2.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Styless.Views
+{
+    /// <summary>
+    /// Interaction logic for Style2.xaml
+    /// </summary>
+    public partial class Style2 : Window
+    {
+        public Style2()
+        {
+            InitializeComponent();
+        }
+    }
+}
